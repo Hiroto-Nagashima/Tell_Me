be08ab36d4eee01be045ad1f6ee99b238c8aaeb7
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-30 00:47:53 +0900

    [add]signupページ作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/organisms/Signup';
 
 function App() {
   return (
     <div className="App">
       Hello World!
       Hello,World
+      <Signup/>
 +     <div>{`REACT_APP_APIKEY:${process.env.REACT_APP_APIKEY}`}</div>
 +     <div>{`REACT_APP_AUTHDOMAIN:${process.env.REACT_APP_AUTHDOMAIN}`}</div>
 +     <div>{`REACT_APP_DATABASEURL:${process.env.REACT_APP_DATABASEURL}`}</div>
