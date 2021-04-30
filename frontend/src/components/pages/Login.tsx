253b595815e3966c9f6435a60b56af760661e1c7
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-30 22:35:37 +0900

    [update]Login.tsxにて値をstateに保存するように変更

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,9 +1,50 @@
-import { VFC } from 'react'
+import { ChangeEvent, MouseEventHandler, useCallback, useContext, useState, VFC } from "react";
+import { withRouter } from "react-router";
+import { AuthContext } from "../../auth/AuthProvider";
+import * as H from 'history'
+import { FormEvent } from 'react';
 
-export const Login:VFC =()=>{
+type Props={
+  history:H.History
+}
+
+export const Login:VFC<Props> =( { history } )=>{
+  const { login } = useContext(AuthContext);
+  const [ email, setEmail ] = useState("")
+  const [ password, setPassword] = useState("")
+
+  const onChangeEmail = useCallback((e:ChangeEvent<HTMLInputElement>)=>{
+    return(
+      setEmail(e.target.value)
+    )
+  },[])
+
+  const onChangePassword = useCallback((e:ChangeEvent<HTMLInputElement>)=>{
+    return(
+      setPassword(e.target.value)
+    )
+  },[])
+
+  // AuthContextからlogin関数を受け取る
+  const handleSubmit = () => {
+    login!(email, password, history);
+  };
+
   return(
     <div>
-      LoginPageです
+      <h1>Log in</h1>
+      <form>
+        <label>
+          Email
+          <input name="email" type="email" placeholder="Email" onChange={onChangeEmail} value={email}/>
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" placeholder="Password" onChange={onChangePassword} value={password} />
+        </label>
+        <button type="submit" onClick={handleSubmit}>Log in</button>
+      </form>
     </div>
   )
 }
+export default withRouter(Login);
