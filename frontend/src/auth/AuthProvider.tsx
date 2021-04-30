9dc91f214cedf1b9be0f7eb3fde2b672a664c625
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-30 14:58:56 +0900

    [add]AuthProvider

---
+++ frontend/src/auth/AuthProvider.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { app } from "../base.js";
+
+// contextの作成
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  // ユーザーをログインさせる関数
+  const login = async (email, password, history) => {
+    try {
+      await app.auth().signInWithEmailAndPassword(email, password);
+      history.push("/");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  // 新しいユーザーを作成しログインさせる関数
+  const signup = async (email, password, history) => {
+    try {
+      await app.auth().createUserWithEmailAndPassword(email, password);
+      history.push("/");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    app.auth().onAuthStateChanged(setCurrentUser);
+  }, []);
+
+  return (
+    // Contextを使用して認証に必要な情報をコンポーネントツリーに流し込む。
+    <AuthContext.Provider
+      value={{
+        login: login,
+        signup: signup,
+        currentUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
