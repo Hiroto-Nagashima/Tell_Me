0c95dd524f95ebaac7f4cd4b00b18cf028e34672
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-02 04:06:21 +0900

    [update]PrivateRoute.tsxのフォーマット整理

--- frontend/src/router/PrivateRoute.tsx
+++ frontend/src/router/PrivateRoute.tsx
@@ -1,16 +1,15 @@
-import { ComponentType, useContext, VFC } from "react";
-import { Route } from "react-router-dom";
-import { AuthContext } from "../auth/AuthProvider";
-import { Login } from "../components/pages/Login";
-import { RouteComponentProps } from 'react-router-dom'
-type Props ={
-  component:ComponentType<RouteComponentProps<number>>
-  exact?: boolean
-  path?:string
-}
+import React, { ComponentType, useContext } from 'react';
+import { Route, RouteComponentProps } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthProvider';
+import { Login } from '../components/pages/Login';
+type Props = {
+  component: ComponentType<RouteComponentProps<number>>;
+  exact?: boolean;
+  path?: string;
+};
 
-const PrivateRoute:VFC<Props> = (props) => {
-  const { component, exact, path} = props
+const PrivateRoute: React.FC<Props> = (props) => {
+  const { component, exact, path } = props;
   const { currentUser } = useContext(AuthContext);
   const Component = currentUser ? component : Login;
 
