e9ec2480f9ffc012f94fa8dcf749bceef38d7062
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-02 15:21:40 +0900

    [update][#8]snackbarのtypeやpropsを整理

---
+++ frontend/src/components/atoms/CustomizedSnackbar.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+// import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+// const Alert = (props: AlertProps) => (
+//   <MuiAlert elevation={6} variant="filled" {...props} />
+// );
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+  severity: 'error' | 'warning' | 'info' | 'success';
+};
+export const CustomizedSnackbar: React.FC<Props> = (props) => {
+  const { open, onClose, severity } = props;
+  const classes = useStyles();
+  // const [open, setOpen] = React.useState(false);
+
+  // const handleClick = () => {
+  //   setOpen(true);
+  // };
+
+  // const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+  //   if (reason === 'clickaway') {
+  //     return;
+  //   }
+
+  //   setOpen(false);
+  // };
+
+  return (
+    <div className={classes.root}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
+        <Alert onClose={onClose} severity={severity}>
+          This is a success message!
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+};
