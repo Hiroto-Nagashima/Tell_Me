096cf1224e199d0b470ae159c2439b58b05ffcaf
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-02 14:17:00 +0900

    [update]airbnb削除

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
     'jest/globals': true,
   },
   extends: [
-    // 'airbnb',
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
@@ -82,25 +81,6 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/prefer-interface': 'off',
 
-    // airbnb
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector: 'ForInStatement',
-        message:
-          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
-      },
-      {
-        selector: 'LabeledStatement',
-        message:
-          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
-      },
-      {
-        selector: 'WithStatement',
-        message:
-          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
-      },
-    ],
     // prefer-arrow
     'prefer-arrow/prefer-arrow-functions': [
       'error',
