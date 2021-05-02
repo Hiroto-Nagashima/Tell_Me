9a7dbb99578a85e75d587d2abd1e3bd6ce1672d8
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-02 14:54:33 +0900

    [add][#8][atoms]複数行のテキストフィールド追加

---
+++ frontend/src/components/atoms/MultipleLineTextField.tsx
@@ -0,0 +1,27 @@
+import React, { ChangeEvent } from 'react';
+import TextField from '@material-ui/core/TextField';
+
+export type Props = {
+  label: string;
+  row: number;
+  value?: string | null;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+// 列の指定ができるTextarea
+export const MultilineTextField: React.FC<Props> = (props) => {
+  const { label, row, value, onChange } = props;
+
+  return (
+    <TextField
+      id="outlined-multiline-static"
+      value={value}
+      onChange={onChange}
+      label={label}
+      multiline
+      rows={row}
+      variant="outlined"
+      style={{ width: '100%' }}
+    />
+  );
+};
