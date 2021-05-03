0232abf55bf09e6fb38f118952f9eacd0ad8337a
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-03 14:27:37 +0900

    [add][#8]MultipleLinesTextFieldのストーリー追加

---
+++ frontend/src/components/atoms/MultipleLinesTextField.stories.tsx
@@ -0,0 +1,34 @@
+import { Story, Meta } from '@storybook/react';
+import { Props, MultipleLinesTextField } from './MultipleLinesTextField';
+import { Grid } from '@material-ui/core';
+
+export default {
+  title: 'atoms/MultipleLinesTextField',
+  component: MultipleLinesTextField,
+} as Meta;
+
+const Template: Story<Props> = (args) => <MultipleLinesTextField {...args} />;
+
+export const Memo = (args: Props) => (
+  <Grid container>
+    <Grid item xs={6}>
+      <MultipleLinesTextField label="連絡事項" row={4} />
+    </Grid>
+  </Grid>
+);
+
+export const Breakfast = (args: Props) => (
+  <Grid container>
+    <Grid item xs={6}>
+      <MultipleLinesTextField label="朝食" row={4} />
+    </Grid>
+  </Grid>
+);
+
+export const Dinner = (args: Props) => (
+  <Grid container>
+    <Grid item xs={6}>
+      <MultipleLinesTextField label="夕食" row={4} />
+    </Grid>
+  </Grid>
+);
