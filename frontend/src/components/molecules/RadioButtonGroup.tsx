0bfeb394b22c972d3eb1fcb011f9d578c39052d3
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-04 01:51:50 +0900

    [update][#10]SignupPaperにてstateを受け取れるようにpropsを変更

--- frontend/src/components/molecules/RadioButtonGroup.tsx
+++ frontend/src/components/molecules/RadioButtonGroup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -6,7 +6,7 @@ import FormControl from '@material-ui/core/FormControl';
 
 type Props = {
   value: 0 | 1;
-  onChange: () => void;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
 export const RadioButtonGroup: React.FC<Props> = (props) => {
--- frontend/src/components/organisms/SignupPaper.tsx
+++ frontend/src/components/organisms/SignupPaper.tsx
@@ -4,6 +4,7 @@ import Paper from '@material-ui/core/Paper';
 import { Box } from '@material-ui/core';
 import { SingleLineTextField } from '../atoms/SingleLineTextField';
 import { FlexibleButton } from '../atoms/FlexibleButton';
+import { RadioButtonGroup } from '../molecules/RadioButtonGroup';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,35 +22,78 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export type Props = {
-  title: string;
-  value: number | null;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  email: string;
+  password: string;
+  parentName: string;
+  radioButtonValue: 0 | 1;
+  telephoneNumber: number;
+  onChangeEmail: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangePassword: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeParentName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeRadioButton: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeTelephoneNumber: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onClick: () => void;
 };
 export const SignupPaper: React.FC<Props> = (props) => {
-  const { title, value, onClick, onChange } = props;
+  const {
+    email,
+    password,
+    parentName,
+    radioButtonValue,
+    telephoneNumber,
+    onChangeEmail,
+    onChangePassword,
+    onChangeParentName,
+    onChangeRadioButton,
+    onChangeTelephoneNumber,
+
+    onClick,
+  } = props;
+
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Paper elevation={3}>
         <Box component="h1" p={2} textAlign="center">
-          {title}
+          Sign UP
+        </Box>
+        <Box textAlign="center" m={4}>
+          <SingleLineTextField
+            textName="お名前"
+            placeholder="お父様かお母様のお名前"
+            value={parentName}
+            onChange={onChangeParentName}
+          />
+        </Box>
+        <Box textAlign="center" m={4}>
+          <RadioButtonGroup
+            value={radioButtonValue}
+            onChange={onChangeRadioButton}
+          />
+        </Box>
+        <Box textAlign="center" m={4}>
+          <SingleLineTextField
+            textName="電話番号"
+            placeholder="xxxxxxx@xxx.ne.jp"
+            value={telephoneNumber}
+            onChange={onChangeTelephoneNumber}
+          />
         </Box>
         <Box textAlign="center" m={4}>
           <SingleLineTextField
             textName="email"
             placeholder="xxxxxxx@xxx.ne.jp"
-            value={value}
-            onChange={onChange}
+            value={email}
+            onChange={onChangeEmail}
           />
         </Box>
         <Box textAlign="center" m={4}>
           <SingleLineTextField
             textName="password"
             placeholder="6文字以上"
-            value={value}
-            onChange={onChange}
+            value={password}
+            onChange={onChangePassword}
           />
         </Box>
         <Box textAlign="center" mx={2}>
