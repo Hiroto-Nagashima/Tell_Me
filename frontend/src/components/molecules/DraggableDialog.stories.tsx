3d7cda426ccd79c77af460d81dd7c62f6176fa74
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-03 14:49:39 +0900

    [add][#9]DraggableDialogのストーリー追加

---
+++ frontend/src/components/molecules/DraggableDialog.stories.tsx
@@ -0,0 +1,26 @@
+import { Story, Meta } from '@storybook/react';
+import { Props, DraggableDialog } from './DraggableDialog';
+
+export default {
+  title: 'Molecules/DraggableDialog',
+  component: DraggableDialog,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '3em' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<Props> = (args) => <DraggableDialog {...args} />;
+
+export const Closed = Template.bind({});
+Closed.args = {
+  isOpen: false,
+};
+
+export const Opened = Template.bind({});
+Opened.args = {
+  isOpen: true,
+};
