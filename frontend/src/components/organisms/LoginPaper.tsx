acaec8334a20cde10bba362e7a78f5401697af62
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-03 23:34:39 +0900

    [add][#10}LoginPaperコンポーネント追加

---
+++ frontend/src/components/organisms/LoginPaper.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import { Box } from '@material-ui/core';
+import { SingleLineTextField } from '../atoms/SingleLineTextField';
+import { FlexibleButton } from '../atoms/FlexibleButton';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      '& > *': {
+        margin: theme.spacing(1),
+        width: theme.spacing(50),
+        height: theme.spacing(60),
+      },
+    },
+  }),
+);
+
+export type Props = {
+  title: string;
+  value: number | null;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onClick: () => void;
+};
+export const LoginPaper: React.FC<Props> = (props) => {
+  const { title, value, onClick, onChange } = props;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper elevation={3}>
+        <Box component="h1" p={2} textAlign="center">
+          {title}
+        </Box>
+        <Box textAlign="center" m={4}>
+          <SingleLineTextField
+            textName="ID"
+            placeholder="1 or 2"
+            value={value}
+            onChange={onChange}
+          />
+        </Box>
+        <Box textAlign="center">
+          <FlexibleButton
+            onClick={onClick}
+            variant="contained"
+            color="primary"
+            label="Login"
+          />
+        </Box>
+      </Paper>
+    </div>
+  );
+};
