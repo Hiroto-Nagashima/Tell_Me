f35646a913a48e4a15d552dc9f902e1b05410ce6
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-03 22:40:54 +0900

    [add][#10}Headerコンポーネント追加

---
+++ frontend/src/components/organisms/Header.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export type Props = {
+  title: string;
+  iconColor: 'inherit' | 'default' | 'primary' | 'secondary';
+  barColor: 'inherit' | 'default' | 'primary' | 'secondary' | 'transparent';
+  name?: string;
+  daycare_name?: string;
+  auth: boolean;
+};
+
+export const MenuAppBar: React.FC<Props> = (props) => {
+  const { title, iconColor, barColor, name, daycare_name, auth } = props;
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color={barColor}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            {title}
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color={iconColor}
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>ようこそ{name}さん</MenuItem>
+                <MenuItem onClick={handleClose}>{daycare_name}</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
