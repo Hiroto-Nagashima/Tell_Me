97626f9364e13cee1d2c128d5820222b1adf6acf
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-05 01:58:19 +0900

    [add][#11]DefaultLayout作成

---
+++ frontend/src/components/templates/DefaultLayout.tsx
@@ -0,0 +1,24 @@
+import React, { memo, ReactNode } from 'react';
+import { Header } from '../organisms/Header';
+import { Footer } from '../organisms/Footer';
+
+type Props = {
+  // タグで囲われているやつはReactNode
+  children: ReactNode;
+};
+export const DefaultLayout: React.FC<Props> = memo((props) => {
+  const { children } = props;
+
+  return (
+    <>
+      <Header auth={false} />
+      {children}
+      <Footer
+        telephoneNumberOfDaycare="080-1111-1234"
+        addressOfDaycare="東京都小平市学園西町1-1-1-1"
+      />
+    </>
+  );
+});
+
+DefaultLayout.displayName = 'DefaultLayout';
