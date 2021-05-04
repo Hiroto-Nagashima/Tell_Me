cf75638ea5fb04b23b8344fbf809d180c01f1b3b
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-05 02:13:36 +0900

    [add][#10][#12]templatesをルーティングに適用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,15 +5,21 @@ import { AuthProvider } from './auth/AuthProvider';
 import { Home } from './components/pages/Home';
 import { Login } from './components/pages/Login';
 import { SignUp } from './components/pages/SignUp';
+import { HeaderLayout } from './components/templates/HeaderLayout';
+import { DefaultLayout } from './components/templates/DefaultLayout';
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Switch>
-          <PrivateRoute exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
+          <DefaultLayout>
+            <PrivateRoute exact path="/" component={Home} />
+          </DefaultLayout>
+          <HeaderLayout>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+          </HeaderLayout>
         </Switch>
       </BrowserRouter>
     </AuthProvider>
