701aa23f8cce76fa34e8d991af894e660ee73a31
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-05 15:46:57 +0900

    [add][#13]Daycateモデル作成

---
+++ backend/app/models/daycare.rb
@@ -0,0 +1,2 @@
+class Daycare < ApplicationRecord
+end
---
+++ backend/db/migrate/20210505064049_create_daycares.rb
@@ -0,0 +1,11 @@
+class CreateDaycares < ActiveRecord::Migration[6.1]
+  def change
+    create_table :daycares do |t|
+      t.string :name
+      t.string :address
+      t.integer :telephone_number
+
+      t.timestamps
+    end
+  end
+end
