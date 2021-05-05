6d1ae883b23e595dac06ccff6ce8d4f3e241a226
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-05 16:07:15 +0900

    [add][#13]Daycareのseedsを追加

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,17 +1,17 @@
 Daycare.create!(
   name: '東京保育園',
-  telephone_number: 08011112222,
+  telephone_number: "08011112222",
   address: "東京都小平市学園西町1-1-1-1"
 )
 
 Daycare.create!(
   name: '神奈川保育園',
-  telephone_number: 08033334444,
+  telephone_number: "08033334444",
   address: "神奈川県横浜市緑区2-2-2-2"
 )
 
 Daycare.create!(
   name: '埼玉保育園',
-  telephone_number: 08055556666,
+  telephone_number: "08055556666",
   address: "埼玉県新座市3-3-3-3"
 )
