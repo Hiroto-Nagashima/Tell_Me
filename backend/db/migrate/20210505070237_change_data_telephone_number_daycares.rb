3d30661725a6471e6f27ddb108033687a4025bd0
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-06 11:20:34 +0900

    [add]rubocupのgemを導入し実行

--- backend/db/migrate/20210505070237_change_data_telephone_number_daycares.rb
+++ backend/db/migrate/20210505070237_change_data_telephone_number_daycares.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeDataTelephoneNumberDaycares < ActiveRecord::Migration[6.1]
   def change
     change_column :daycares, :telephone_number, :string
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 Daycare.create!(
   name: '東京保育園',
-  telephone_number: "08011112222",
-  address: "東京都小平市学園西町1-1-1-1"
+  telephone_number: '08011112222',
+  address: '東京都小平市学園西町1-1-1-1'
 )
 
 Daycare.create!(
   name: '神奈川保育園',
-  telephone_number: "08033334444",
-  address: "神奈川県横浜市緑区2-2-2-2"
+  telephone_number: '08033334444',
+  address: '神奈川県横浜市緑区2-2-2-2'
 )
 
 Daycare.create!(
   name: '埼玉保育園',
-  telephone_number: "08055556666",
-  address: "埼玉県新座市3-3-3-3"
+  telephone_number: '08055556666',
+  address: '埼玉県新座市3-3-3-3'
 )
