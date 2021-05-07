5cfc0c95cfa4526d9cd2433cb7fe7f19738ebd04
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-07 12:07:03 +0900

    [add][#16]JWT認証に必要なコードをapplication_controllerに記載

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  module Api
+    class V1::ApplicationController < ActionController::API
+      include Firebase::Auth::Authenticable
+      before_action :authenticate_user
+    end
+  end
 end
