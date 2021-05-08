dd27b2f66311fed0eacaf28a53f06209a41a5f32
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-08 11:38:01 +0900

    [add][#16]parent_controllerにcreateアクション追加

--- backend/app/controllers/api/v1/parents_controller.rb
+++ backend/app/controllers/api/v1/parents_controller.rb
@@ -1,8 +1,22 @@
 module Api
   module V1
     class ParentsController < Api::V1::ApplicationController
+      include FirebaseAuthConcern
+      before_action :set_auth, only: %i[create update]
+
+      include CreateUserConcern
+      def create
+        create_user(@auth)
+      end
+
       def show
         render json: current_parent, status: 200
+      end
+
+      private
+
+      def set_auth
+        @auth = authenticate_token_by_firebase
       end
     end
   end
