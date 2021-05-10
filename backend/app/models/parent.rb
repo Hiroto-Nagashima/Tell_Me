67b92026df9c13a880e682bb952d59a7f4720cb6
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-11 02:10:12 +0900

    [add][#24]backend側でParentモデルのバリデーション追加

--- backend/app/models/parent.rb
+++ backend/app/models/parent.rb
@@ -1,3 +1,7 @@
 class Parent < ApplicationRecord
   belongs_to :kid, optional: true
+
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
 end
