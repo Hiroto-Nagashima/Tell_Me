ce7509bf2802441f2f123465059cfb8bf6ca0c80
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-11 13:24:49 +0900

    [update][#24]Parentモデル内の空白削除しエラー解決

--- backend/app/models/parent.rb
+++ backend/app/models/parent.rb
@@ -6,7 +6,7 @@ class Parent < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
-　VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
-　validates :telephone_number, format: { with: VALID_PHONE_REGEX }
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  validates :telephone_number, format: { with: VALID_PHONE_REGEX }
 
 end
