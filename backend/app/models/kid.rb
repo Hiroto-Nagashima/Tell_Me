ae9fb28760b6a3badf07f2448e2c3f55ef0adbd9
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-11 10:20:39 +0900

    [add][#24]Kidのバリデーション追加

--- backend/app/models/kid.rb
+++ backend/app/models/kid.rb
@@ -1,4 +1,7 @@
 class Kid < ApplicationRecord
   belongs_to :daycare
   has_many :parents
+　
+　validates :first_name, :last_name, :age, :gender, :daycare_id, :favorite_food, :favorite_play, presence: true
+  validates :age, :daycare_id, numericality: true
 end
