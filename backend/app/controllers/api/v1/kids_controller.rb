31a1d88d13dab9702bd6db5e955395caebc76f71
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-11 13:48:18 +0900

    [update][#27]kidを登録するコントローラー作成

---
+++ backend/app/controllers/api/v1/kids_controller.rb
@@ -0,0 +1,33 @@
+module Api
+  module V1
+    class KidsController < ApplicationController
+      def create
+        daycare = Daycare.find(params[:daycare_id])
+        kid = daycare.kids.build(kid_params)
+        if kid.save
+          parent = Parent.find_by(uid: params[:uid])
+          parent.kid_id = kid.id
+          render json: {
+            status: "ok"
+          }
+        else
+          render json: {
+            status: 400,
+            message: "未入力箇所に誤りがあります"
+          }
+        end
+      end
+
+      def index
+　　　　　kids = Kids.all
+        render json: kid, status: 200
+      end
+
+      private
+
+      def kid_params
+        params.require(:params).permit(:age, :first_name, :last_name, :gender, :favorite_food, :favorite_play, :uid)
+      end
+    end
+  end
+end
