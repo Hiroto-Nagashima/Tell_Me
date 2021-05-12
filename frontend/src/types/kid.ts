c8daa002dbe5e995f353fd9c1186e85c40ddefdc
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-12 22:16:37 +0900

    [update][#28]Kidの型定義をtypesに切り出し

---
+++ frontend/src/types/kid.ts
@@ -0,0 +1,12 @@
+export type Kid = {
+  age: number | null;
+  gender: number;
+  parentName: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  daycareID: number | null;
+  favoriteFood: string | null;
+  favoritePlay: string | null;
+  created_at: string;
+  updated_at: string;
+};
