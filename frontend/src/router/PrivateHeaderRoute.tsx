a159fbf823baf9c604ec350938d234515851f957
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-12 14:23:53 +0900

    [add][#29]HeaderレイアウトのPrivateRoute作成

---
+++ frontend/src/router/PrivateHeaderRoute.tsx
@@ -0,0 +1,41 @@
+import React, { ComponentType } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Route, RouteComponentProps } from 'react-router-dom';
+import { Login } from '../components/pages/Login';
+import { HeaderLayout } from '../components/templates/HeaderLayout';
+import { getAuth } from '../helper/firebaseAuthHelper';
+
+type Props = {
+  component: ComponentType<RouteComponentProps>;
+  exact: boolean;
+  path: string;
+};
+
+const PrivateHeaderRoute: React.FC<Props> = ({
+  component: Component,
+  ...rest
+}) => {
+  const [user] = useAuthState(getAuth());
+
+  return user ? (
+    <Route
+      {...rest}
+      render={(props) => (
+        <HeaderLayout>
+          <Component {...props} />
+        </HeaderLayout>
+      )}
+    />
+  ) : (
+    <Route
+      exact
+      path={'/'}
+      render={(props) => (
+        <HeaderLayout>
+          <Login {...props} />
+        </HeaderLayout>
+      )}
+    />
+  );
+};
+export default PrivateHeaderRoute;
