e3d15a949a09c07749a67e25c7dce8c0b7d402e3
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-16 02:12:23 +0900

    [add][#38]Notebookモデルでアソシエーション追加

--- backend/app/models/kid.rb
+++ backend/app/models/kid.rb
@@ -2,6 +2,7 @@ class Kid < ApplicationRecord
   belongs_to :daycare
   has_many :parents, through: :kids_parents
   has_many :kid_parents
+  has_many :notebooks
   validates :first_name, :last_name, :age, :gender, :daycare_id, :favorite_food, :favorite_play, presence: true
   validates :age, :daycare_id, numericality: true
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,12 +3,12 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :daycares, only: [:show] do
+      resources :daycares, only: [:show]
+      resources :kids, only: [:index, :create, :update] do
         get '/communication_notebooks/doesExist', to: 'communication_notebooks#doesExist'
         get '/communication_notebooks/findByDate', to: 'communication_notebooks#findByDate'
         resources :communication_notebooks, only: [:index, :create, :update]
       end
-      resources :kids, only: [:index, :create, :update]
       get '/parents/fetchParentName', to: 'parents#fetchParentName'
       get '/parents/fetchParentArray', to: 'parents#fetchParentArray'
       resources :parents, only: [:create, :update]
