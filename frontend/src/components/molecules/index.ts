14efce4a4bca6337148ded03442f9955dc5c55a5
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-15 14:36:22 +0900

    [add][#36]moleculesのindex.ts追加

---
+++ frontend/src/components/molecules/index.ts
@@ -0,0 +1,6 @@
+import { KidCard } from './KidCard/KidCard';
+import { DatePicker } from './DatePicker/DatePicker';
+import { DraggableDialog } from './DraggableDialog/DraggableDialog';
+import { RadioButtonGroup } from './RadioButtonGroup';
+
+export { KidCard, DatePicker, DraggableDialog, RadioButtonGroup };
