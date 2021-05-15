82da9387ef4daf419190ad3aa4c242c45311c63d
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-15 14:44:22 +0900

    [add][#36]organismsのindex.ts追加

---
+++ frontend/src/components/organisms/index.ts
@@ -0,0 +1,21 @@
+import { Post } from './Post';
+import { Footer } from './Footer/Footer';
+import { Header } from './Header/Header';
+import { KidProfile } from './KidProfile/KidProfile';
+import { LoginPaper } from './LoginPaper/LoginPaper';
+import { SignUpPaper } from './SignUpPaper/SignUpPaper';
+import { ParentProfile } from './ParentProfile/ParentProfile';
+import { InputOfNotebook } from './InputOfNotebook/InputOfNotebook';
+import { RegisterKidPaper } from './RegisterKidPaper/RegisterKidPaper';
+
+export {
+  Post,
+  Footer,
+  Header,
+  KidProfile,
+  LoginPaper,
+  SignUpPaper,
+  ParentProfile,
+  InputOfNotebook,
+  RegisterKidPaper,
+};
