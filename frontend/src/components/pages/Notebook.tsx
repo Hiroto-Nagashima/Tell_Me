de4672d0c1b928eb01ddb16982dd6fe68a29f0f3
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-16 14:21:23 +0900

    [add][#40]Notebookコンポーネント内にerrorの場合とローフォ中の処理を追加

--- frontend/src/components/pages/Notebook.tsx
+++ frontend/src/components/pages/Notebook.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { DefaultButton } from '../atoms';
+import { DefaultButton, Spinner } from '../atoms';
 import { DatePicker } from '../molecules';
 
 export const Notebook: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
   const handleDateChange = (date: Date | null) => {
     setSelectedDate(date);
@@ -11,14 +13,25 @@ export const Notebook: React.FC = () => {
 
   return (
     <>
-      <div>日付を選択してください</div>
-      <DatePicker onChangeDate={handleDateChange} selectedDate={selectedDate} />
-      <DefaultButton
-        variant="contained"
-        color="primary"
-        label="連絡帳を確認"
-        size="medium"
-      />
+      {loading ? (
+        <Spinner />
+      ) : error ? (
+        <div>error</div>
+      ) : (
+        <>
+          <div>日付を選択してください</div>
+          <DatePicker
+            onChangeDate={handleDateChange}
+            selectedDate={selectedDate}
+          />
+          <DefaultButton
+            variant="contained"
+            color="primary"
+            label="連絡帳を確認"
+            size="medium"
+          />
+        </>
+      )}
     </>
   );
 };
