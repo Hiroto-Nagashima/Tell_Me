87caf27fe96b7f91719dd341ef1eb3523bc4ae17
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-16 22:17:05 +0900

    [add][#40]Notebook.tsxのボタンクリック時のaxios追加

--- frontend/src/components/pages/Notebook.tsx
+++ frontend/src/components/pages/Notebook.tsx
@@ -1,14 +1,28 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { DefaultButton, Spinner } from '../atoms';
 import { DatePicker } from '../molecules';
 
 export const Notebook: React.FC = () => {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState('');
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
   const handleDateChange = (date: Date | null) => {
     setSelectedDate(date);
     console.log(date);
+  };
+
+  const onClickCheck = () => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5000/api/v1/kids/#{kid.id}/notebook`, {
+        params: {
+          date: selectedDate,
+        },
+      })
+      .then((res) => console.log(res.data))
+      .catch((e) => setError(e))
+      .finally(() => setLoading(false));
   };
 
   return (
@@ -16,7 +30,7 @@ export const Notebook: React.FC = () => {
       {loading ? (
         <Spinner />
       ) : error ? (
-        <div>error</div>
+        <div>{error}</div>
       ) : (
         <>
           <div>日付を選択してください</div>
@@ -29,6 +43,7 @@ export const Notebook: React.FC = () => {
             color="primary"
             label="連絡帳を確認"
             size="medium"
+            onClick={onClickCheck}
           />
         </>
       )}
