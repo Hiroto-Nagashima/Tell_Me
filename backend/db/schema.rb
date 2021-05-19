c1aaef3ecbd509c2ba5390bac8db815eb0ac3afb
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-20 00:31:38 +0900

    [add][#49]usersテーブルにdaycare_id追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_132208) do
+ActiveRecord::Schema.define(version: 2021_05_19_153006) do
 
   create_table "daycares", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_132208) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "uid"
+    t.integer "daycare_id"
   end
 
 end
