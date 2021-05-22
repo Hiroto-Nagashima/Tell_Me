e077c2f503a408c0cafb550561d0f38ca3c47649
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-22 16:25:43 +0900

    [update]ChooseKidのコードを整理

--- frontend/src/components/pages/ChooseKid.tsx
+++ frontend/src/components/pages/ChooseKid.tsx
@@ -1,28 +1,27 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Kid } from '../../types/api/kid';
 import { Spinner } from '../atoms/Spinner/Spinner';
 import { KidCard } from '../molecules/KidCard/KidCard';
-import axios from 'axios';
 import { getAuth } from '../../helper/firebaseAuthHelper';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Kid } from '../../types/api/kid';
 import { useHistory } from 'react-router';
-import { CurrentUserContext } from '../../providers/UserProvider';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export const ChooseKid: React.FC = () => {
-  console.log(4);
+  const history = useHistory();
+
+  const [user] = useAuthState(getAuth());
+
   const [kids, setKids] = useState<Array<Kid>>();
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [user] = useAuthState(getAuth());
-  const history = useHistory();
-  const { currentUser } = useContext(CurrentUserContext);
-  console.log(currentUser);
 
   const onClickButton = (kidId: number) => {
     history.push(`/kids/${kidId}`);
   };
-  console.log(6);
-  const fetchKid = () =>
+
+  const fetchKid = () => {
+    setLoading(true);
     axios
       .get('http://localhost:5000/api/v1/kids', {
         params: {
@@ -34,16 +33,13 @@ export const ChooseKid: React.FC = () => {
           history.push('/kids/register');
         } else {
           setKids(res.data);
-          console.log(res);
         }
       })
       .catch((e) => setError(e))
       .finally(() => setLoading(false));
-  console.log(8);
+  };
 
   useEffect(() => {
-    console.log(1);
-    console.log('Choose！！！！');
     fetchKid();
   }, []);
 
@@ -57,8 +53,6 @@ export const ChooseKid: React.FC = () => {
         <>
           <p>お子様を選択してください</p>
           {kids?.map((kid) => {
-            console.log(kid);
-
             return (
               <div key={kid.id}>
                 <KidCard
