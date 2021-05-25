7220670fd0b8e11d2d60b56261b86b808226c9d9
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-25 10:14:45 +0900

    [update][#61]createアクションを更新

--- backend/app/controllers/api/v1/posts_controller.rb
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -3,7 +3,16 @@ module Api
     class PostsController < ApplicationController
       def create
         daycare = Daycare.find(params[:daycare_id])
-        teacher = User.find(params[:user_id])
+        user = User.find(params[:user_id])
+        post = Post.new
+        post.daycare_id = daycare.id
+        post.user_id = user.id
+        if post.save!
+          render json: post, status: "ok"
+        else
+          render json: {
+            status: 400,
+          }
       end
 
       def all_posts
