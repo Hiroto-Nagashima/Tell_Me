938f9fc41d482f9c81530cfa7467a0d34136ebfe
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-26 01:55:07 +0900

    [update][#66]HomeにてSVG画像導入

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,26 +1,34 @@
 import React, { ChangeEvent, useCallback, useContext, useState } from 'react';
 import * as H from 'history';
 import axios from 'axios';
-import Grid from '@material-ui/core/Grid';
-import Image from '../../images/kid.jpeg';
+// import Grid from '@material-ui/core/Grid';
 import styled from 'styled-components';
 import firebase from 'firebase';
+import { ReactComponent as Logo } from '../../images/undraw_Notebook_re_id0r.svg';
 import { getAuth } from '../../helper/firebaseAuthHelper';
 import { useHistory } from 'react-router-dom';
 import { CustomizedSnackbar } from '../atoms/CustomizedSnackbar/CustomizedSnackbar';
 import { LoginPaper } from '../organisms/LoginPaper/LoginPaper';
 import { CurrentUserContext } from '../../providers/UserProvider';
 import { Spinner } from '../atoms';
+import { Box } from '@material-ui/core';
 
 type Props = {
   history: H.History;
 };
-const BackgroundImage = styled.div`
-  background-image: url(${Image});
-  background-size: cover;
-  background-position: center;
-  height: 100vh;
-  padding-top: 120px;
+
+const HomeImage = styled(Logo)`
+  width: 800px;
+`;
+
+const Wrapper = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  padding: 8% 5% 5% 5%;
+`;
+
+const MyLoginPaper = styled(LoginPaper)`
+  margin: 60px;
 `;
 
 export const Login: React.FC<Props> = () => {
@@ -94,26 +102,23 @@ export const Login: React.FC<Props> = () => {
         <Spinner />
       ) : (
         <>
-          <BackgroundImage>
-            <Grid container justify="flex-end" alignItems="flex-end">
-              <Grid item xs={6}>
-                <LoginPaper
-                  email={email}
-                  onChangeEmail={onChangeEmail}
-                  password={password}
-                  onChangePassword={onChangePassword}
-                  onClickLogin={handleSubmit}
-                />
-              </Grid>
-            </Grid>
-            <CustomizedSnackbar
-              open={open}
-              onClose={handleClose}
-              severity="error"
-            >
-              {error}
-            </CustomizedSnackbar>
-          </BackgroundImage>
+          <Wrapper>
+            <HomeImage />
+            <MyLoginPaper
+              email={email}
+              onChangeEmail={onChangeEmail}
+              password={password}
+              onChangePassword={onChangePassword}
+              onClickLogin={handleSubmit}
+            />
+          </Wrapper>
+          <CustomizedSnackbar
+            open={open}
+            onClose={handleClose}
+            severity="error"
+          >
+            {error}
+          </CustomizedSnackbar>
         </>
       )}
     </>
