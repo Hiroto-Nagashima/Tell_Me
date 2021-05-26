2580d7c9b5d69fd9500acb727f6ed27e2c18531d
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-26 21:49:07 +0900

    [update][#67]UserProviser内の不要なstate削除、コード整形

--- frontend/src/providers/UserProvider.tsx
+++ frontend/src/providers/UserProvider.tsx
@@ -1,9 +1,10 @@
-import axios from 'axios';
 import React, { createContext, ReactNode, useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Spinner } from '../components/atoms';
+import axios from 'axios';
 import { getAuth } from '../helper/firebaseAuthHelper';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { CurrentUser } from '../types/frontend/currentUser';
+
+import { Spinner } from '../components/atoms';
 
 type Props = {
   children: ReactNode;
@@ -13,20 +14,21 @@ export const CurrentUserContext = createContext(
   {} as {
     currentUser: CurrentUser;
     setCurrentUser: React.Dispatch<React.SetStateAction<CurrentUser>>;
-    loadingCurrentUser: boolean;
   },
 );
 
 export const UserProvider: React.FC<Props> = (props) => {
   const { children } = props;
+
+  const [user, loading] = useAuthState(getAuth());
+
+  const [error, setError] = useState(false);
+
   const [currentUser, setCurrentUser] = useState<CurrentUser>(
     {} as CurrentUser,
   );
-  const [loadingCurrentUser, setLoadingCurrentUser] = useState(false);
-  const [user, loading] = useAuthState(getAuth());
 
   const fetchUser = async () => {
-    setLoadingCurrentUser(true);
     if (user) {
       axios
         .get(`http://localhost:5000/api/v1/users/fetch_user`, {
@@ -35,20 +37,15 @@ export const UserProvider: React.FC<Props> = (props) => {
           },
         })
         .then((res) => {
-          console.log('hahahaha');
           setCurrentUser(res.data.user);
-          setLoadingCurrentUser(false);
         })
-        .catch((e) => console.log(e));
+        .catch((e) => setError(e));
     } else {
-      console.log('fugafuga');
-
       return null;
     }
   };
 
   useEffect(() => {
-    console.log('heyo');
     !loading && fetchUser();
   }, [loading]);
 
@@ -56,10 +53,12 @@ export const UserProvider: React.FC<Props> = (props) => {
     return <Spinner />;
   }
 
+  if (error) {
+    return <div>エラーです</div>;
+  }
+
   return (
-    <CurrentUserContext.Provider
-      value={{ currentUser, setCurrentUser, loadingCurrentUser }}
-    >
+    <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
       {children}
     </CurrentUserContext.Provider>
   );
