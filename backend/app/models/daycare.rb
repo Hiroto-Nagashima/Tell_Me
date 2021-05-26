a9e88239cc6dade236da9860e052ea30ccdbfd13
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-26 11:29:27 +0900

    [update][#67]model内のコード整形

--- backend/app/models/daycare.rb
+++ backend/app/models/daycare.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Daycare < ApplicationRecord
   has_many :kids
   has_many :posts
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
   enum role: { 保護者: 0, 先生: 1 }
+
   belongs_to :daycare, optional: true
   has_many :kids, through: :kid_users
   has_many :kid_users
   has_many :posts
-  validates :first_name, :last_name, :telephone_number, :uid, :email, :password, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
   validates :telephone_number, format: { with: VALID_PHONE_REGEX }
+  validates :first_name, :last_name, :telephone_number, :uid, :email, :password, presence: true
+
   mount_base64_uploader :image, ImageUploader
 end
