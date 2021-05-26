e87a4eaee9970125573dd376c98023f847db1cca
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-26 11:04:19 +0900

    [update][#67]usersコントローラーにて不要なグローバル変数削除

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -26,20 +26,20 @@ module Api
       end
 
       def fetch_user
-        @user = User.find_by(uid: params[:uid])
+        user = User.find_by(uid: params[:uid])
         render json: {
           user:{
-            id: @user.id,
-            role: @user.role,
-            email: @user.email,
-            image: @user.image,
-            daycareId: @user.daycare_id,
-            password: @user.password,
-            gender: @user.gender,
-            firstName: @user.first_name,
-            lastName: @user.last_name,
-            telephoneNumber: @user.telephone_number,
-            selfIntroduction: @user.self_introduction
+            id: user.id,
+            role: user.role,
+            email: user.email,
+            image: user.image,
+            daycareId: user.daycare_id,
+            password: user.password,
+            gender: user.gender,
+            firstName: user.first_name,
+            lastName: user.last_name,
+            telephoneNumber: user.telephone_number,
+            selfIntroduction: user.self_introduction
           }
         }, status: 200
       end
