1ed558ec5eb7948430d90578eab388de75c511e2
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-28 02:21:29 +0900

    [error][#69]Kidモデルの正常系のテストでアソシエーション先のモデルを求められ失敗

--- backend/spec/models/kid_spec.rb
+++ backend/spec/models/kid_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper.rb'
 
 RSpec.describe Kid, type: :model do
+  describe '正常系の機能' do
+    it '名前、年齢、保育園名、メールアドレスがあれば成功' do
+      kid = Kid.new(
+        age: 1,
+        gender:1,
+        last_name: '山田',
+        first_name: '太郎',
+        daycare_id: 1,
+        favorite_food: '寿司',
+        favorite_play: '野球'
+      )
+      expect(kid).to be_valid
+      kid.save
+    end
+  end
+
   describe '入力値の有無' do
     it '姓が空欄の場合保存に失敗するか' do
       kid = Kid.new(first_name: nil)
@@ -20,7 +36,7 @@ RSpec.describe Kid, type: :model do
       expect(kid.errors.messages[:age]).to include("can't be blank")
     end
 
-    it '父か母かの選択欄が空欄の場合保存に失敗するか' do
+    it '性別が空欄の場合保存に失敗するか' do
       kid = Kid.new(gender: nil)
       expect(kid). not_to be_valid
       expect(kid.errors.messages[:gender]).to include("can't be blank")
@@ -44,4 +60,5 @@ RSpec.describe Kid, type: :model do
       expect(kid.errors.messages[:favorite_play]).to include("can't be blank")
     end
   end
+
 end
