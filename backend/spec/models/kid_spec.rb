005573c1bdd65ad15990bab65e3628b2411685ca
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-28 01:30:22 +0900

    [add][#69]rspecの最初のテストを通すことに成功

--- backend/spec/models/kid_spec.rb
+++ backend/spec/models/kid_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
+require 'spec_helper.rb'
 
 RSpec.describe Kid, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '入力値の有無' do
+    it '名前が空欄の場合保存に失敗するか' do
+        kid = Kid.new
+        expect(kid). not_to be_valid
+        expect(kid.errors.messages[:first_name]).to include("can't be blank")
+      end
+
+  end
 end
