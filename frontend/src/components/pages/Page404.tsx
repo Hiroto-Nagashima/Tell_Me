5706a9cc7533f59d6a197293be8715316f0c8c76
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-05-29 03:53:03 +0900

    [add][#70]Page404ページに画像導入

--- frontend/src/components/pages/Page404.tsx
+++ frontend/src/components/pages/Page404.tsx
@@ -1,5 +1,16 @@
+import { Box, Typography } from '@material-ui/core';
 import React from 'react';
+import { ReactComponent as Logo } from '../../images/undraw_page_not_found_su7k.svg';
 
 export const Page404: React.FC = () => {
-  return <div>エラーです</div>;
+  return (
+    <Box p={10}>
+      <Typography variant="h2" align="center">
+        Not Found
+      </Typography>
+      <Box textAlign="center">
+        <Logo />
+      </Box>
+    </Box>
+  );
 };
