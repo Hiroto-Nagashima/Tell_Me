3b0745b65a958d9689348d2b16bf02ce5beef084
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-06-01 01:52:15 +0900

    [add][#75]連絡帳の作成と更新のテスト成功

--- backend/spec/requests/api/v1/notebooks_spec.rb
+++ backend/spec/requests/api/v1/notebooks_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe "Api::V1::Notebooks", type: :request do
     end
   end
 
+  describe "POST /api/v1/kids/:id/notebooks/" do
+    it 'ある子供の連絡帳を登録' do
+      kid = create(:kid)
+      valid_params= attributes_for(:notebook)
+      expect { post "/api/v1/kids/#{kid.id}/notebooks", params:{notebook: valid_params }}
+      .to change(Notebook, :count).by(+1)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "PUT /api/v1/kids/:kid_id/notebooks/:id" do
+    it 'ある子供の連絡帳を更新' do
+      kid = create(:kid)
+      target_notebook = create(:notebook, kid_id: kid.id, memo: "普通です", date: "2021-4-26")
+      put "/api/v1/kids/#{kid.id}/notebooks/#{target_notebook.id}", params: {notebook: {memo: "元気です"}}
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(target_notebook.reload.memo).to eq("元気です")
+      expect(json['message']).to eq("更新が完了しました")
+    end
+  end
+
 end
