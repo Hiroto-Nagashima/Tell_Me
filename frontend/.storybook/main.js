3a57af7a6bb691b291a14bdf0c09aee71d724f8a
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-06-02 17:26:00 +0900

    [fix]storybookの立ち上げ成功

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -7,5 +7,15 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
 }
+
