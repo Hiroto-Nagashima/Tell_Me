9db0f8277eb7a114b2cadb90bacb2ce76ed91c45
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-06-14 04:32:27 +0900

    [update]apiの修正をrspecに反映

--- backend/spec/requests/api/v1/posts_spec.rb
+++ backend/spec/requests/api/v1/posts_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Posts", type: :request do
-  describe "GET /daycares/:daycare_id/users/:id/posts/user_posts" do
+  describe "GET /daycares/:daycare_id/users/:id/posts" do
     it 'ある保育園の先生の投稿を全て取得' do
       daycare = create(:daycare)
       user = create(:user)
       posts = create_list(:post, 10, daycare_id:daycare.id, user_id: user.id)
-      get "/api/v1/daycares/#{daycare.id}/users/#{user.id}/posts/user_posts"
+      get "/api/v1/daycares/#{daycare.id}/users/#{user.id}/posts"
       json = JSON.parse(response.body)
       expect(response.status).to eq(200)
       expect(json.size).to eq 10
     end
   end
 
-  describe "GET /daycares/:id/posts/all_posts" do
+  describe "GET /daycares/:id/posts" do
     it 'ある保育園の投稿を全て取得' do
       daycare = create(:daycare)
       posts = create_list(:post, 20, daycare_id:daycare.id)
-      get "/api/v1/daycares/#{daycare.id}/posts/all_posts"
+      get "/api/v1/daycares/#{daycare.id}/posts"
       json = JSON.parse(response.body)
       expect(response.status).to eq(200)
       expect(json.size).to eq 20
