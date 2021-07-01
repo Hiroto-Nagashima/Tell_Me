0c0245950d181b7df7253bef5f69577618646d88
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-07-01 13:02:53 +0900

    [update][#96]notebook_templatesコントローラーのアクションを更新

--- backend/app/controllers/api/v1/notebook_templates.rb
+++ backend/app/controllers/api/v1/notebook_templates.rb
@@ -2,14 +2,30 @@ module Api
   module V1
     class NotebookTemplatesController < ApplicationController
       def create
+        kid = Kid.find(params[:kid_id])
+        notebook_templates = kid.notebook_templates.build(notebook_templates_params)
+        if notebook_templates.save!
+          render json: {
+            status: 200,
+            message: "登録が完了しました"
+          }
+        else
+          render json: {
+            status: "422",
+            message: notebook_templates.errors.full_messages
+          }
+        end
       end
 
       def show
+        kid = Kid.find(params[:kid_id])
+        notebook_templates = NotebookTemplates.where(kid_id: kid.id, id: params[:id])
+        render json: notebook_templates
       end
 
       private
 
-      def notebook_params
+      def notebook_templates_params
         params.require(:notebook_templates).permit(:has_bathed, :breakfast, :dinner)
       end
     end
