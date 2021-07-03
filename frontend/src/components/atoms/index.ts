b953ddf2b71bb4157cddb80cdc45b3661efab8f1
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-07-03 23:24:31 +0900

    [update][#98]DatePickerをatomsに移動

--- frontend/src/components/atoms/index.ts
+++ frontend/src/components/atoms/index.ts
@@ -1,15 +1,17 @@
 import { Spinner } from './Spinner/Spinner';
+import { TextField } from './TextField/TextField';
+import { DatePicker } from '../atoms/DatePicker/DatePicker';
+import { SelectMenu } from './SelectMenu/SelectMenu';
 import { StyledButton } from './StyledButton/StyledButton';
 import { DraggablePaper } from '../organisms/DraggableDialog/DraggablePaper';
 import { CustomizedSnackbar } from './CustomizedSnackbar/CustomizedSnackbar';
 import { RadioButtonGroup } from '../atoms/RadioButtonGroup/RadioButtonGroup';
 
-import { TextField } from './TextField/TextField';
-import { SelectMenu } from './SelectMenu/SelectMenu';
 
 export {
   Spinner,
   TextField,
+  DatePicker,
   SelectMenu,
   StyledButton,
   DraggablePaper,
--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -1,11 +1,9 @@
-import { DatePicker } from './DatePicker/DatePicker';
 import { AlignedTextFields } from './AlignedTextFields/AlignedTextFields';
 import { LabeledTextField } from './LabeledTextField/LabeledTextField';
 import { LabeledSelectMenu } from './LabeledSelectMenu/LabeledSelectMenu';
 import { LabeledRadioButtonGroup } from './LabeledRadioButtonGroup/LabeledRadioButtonGroup';
 
 export {
-  DatePicker,
   LabeledTextField,
   LabeledSelectMenu,
   LabeledRadioButtonGroup,
