98a4778df7e48dee72df1c5228b5470a4372c21f
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-07-15 23:57:41 +0900

    [update]contextを追加

--- backend/spec/requests/api/v1/daycares_spec.rb
+++ backend/spec/requests/api/v1/daycares_spec.rb
@@ -4,12 +4,14 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Daycares', type: :request do
   describe 'GET /daycares/:id' do
-    it 'IDをもとに保育園のレコードを一件取得' do
-      daycare = create(:daycare, address: '東京都小平市')
-      get "/api/v1/daycares/#{daycare.id}"
-      json = JSON.parse(response.body)
-      expect(response).to have_http_status(200)
-      expect(json['daycare']['address']).to eq('東京都小平市')
+    context '成功時のレスポンス' do
+      it 'IDをもとに保育園のレコードを一件取得' do
+        daycare = create(:daycare, address: '東京都小平市')
+        get "/api/v1/daycares/#{daycare.id}"
+        json = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(json['daycare']['address']).to eq('東京都小平市')
+      end
     end
   end
 end
